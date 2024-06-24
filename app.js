f8bec5afff3974d151c7b71ce6250c6447963350
dens <dens@News-MacBook-Pro.local>
2024-06-23 19:23:03 -0500

    123

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import transactionRoutes from "./routes/transactionRoutes.js";
 import countryRoutes from "./routes/countryRoutes.js";
 import walletRoutes from "./routes/walletRoutes.js";
 import path from 'path'
-// prueba33322222 22
+// prueba bien
 dotenv.config();
 connectDB();
 
