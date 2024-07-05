92877ca48ed091a10b2faab07571b7925d68c893
dens <dens@News-MacBook-Pro.local>
2024-07-04 22:18:37 -0500

    1111

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,8 @@ router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
 router.get('/verify/:token', verifyEmail);
 router.post('/reset-password/:token', resetPassword);
+
+
 // Ruta protegida para obtener el perfil del usuario
 router.get('/profile/:id', authMiddleware, profile); // Modificado para incluir el parámetro de ID
 
