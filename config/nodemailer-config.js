546574da0e1915239fd23ecd7305a6bbe8840916
dens <dens@News-MacBook-Pro.local>
2024-07-05 14:34:07 -0500

    1111

--- config/nodemailer-config.js
+++ config/nodemailer-config.js
@@ -3,7 +3,10 @@ import nodemailer from 'nodemailer';
 
 // Configuración básica de Nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'email-smtp.us-east-2.amazonaws.com',
+  host: 'email-smtp.us-east-2.amazonaws.com',
+  port: 587, 
+  secure: false, 
+  //service: 'email-smtp.us-east-2.amazonaws.com',
   auth: {
     user: 'account@baseproject.org',
     pass: '16944615*Dd'
